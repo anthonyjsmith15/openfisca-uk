a37de93ac99596e33a726a644edd3ee32603a4c8
Anthony Smith <112818446+anthonyjsmith15@users.noreply.github.com>
2023-08-16 22:03:44 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
-export COUNTRY_NAME=UK  # set the name of your country here; you should keep all capitals, and replace any spaces in the name by underscores
-export REPOSITORY_URL=https://github.com/YOUR_ORGANISATION/OpenFisca-$COUNTRY_NAME  # set here the URL of the repository you created in step 2.
-./bootstrap.sh
+export COUNTRY_NAME=uk  # set the name of your country here; you should keep all capitals, and replace any spaces in the name by underscores
+export REPOSITORY_URL=https://github.com/anthonyjsmith15/OpenFisca-$COUNTRY_NAME  # set here the URL of the repository you created in step 2.
+
